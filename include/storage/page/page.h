9124e05f5d51b5edcad1cc7253aae653d8ce3820
aguspee <165193270+aguspee@users.noreply.github.com>
2026-03-17 15:01:28 -0300

    ,

--- include/storage/page/page.h
+++ include/storage/page/page.h
@@ -4,5 +4,19 @@
 class Page{
     public: 
     static constexpr uint16_t PAGE_SIZE= 8192;
+
+    struct PageHeader{
+        uint16_t slot_count;
+        uint16_t free_space;
+
+    };
     
-}
+    struct Slot{
+        uint16_t  offset;
+        uint16_t length;
+        uint8_t deleted;
+    };
+
+    Page();
+
+};
