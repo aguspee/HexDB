a1ce4e21e5bc6289b32c7adb2e76aa55e27eb492
aguspee <agusperez2255@gmail.com>
2026-03-22 11:21:56 -0300

    page.cpp

--- src/storage/page/page.cpp
+++ src/storage/page/page.cpp
@@ -6,3 +6,33 @@
 Page :: Page(){
     initialize();
 }
+void Page:: initialize(){
+    std:: memset(data, 0, PAGE_SIZE);
+    PageHeader header{};
+    header.slot_count= 0;
+    header.free_space=PAGE_SIZE;
+    setHeader(header);
+
+}
+
+Page::PageHeader Page::getHeader(){
+    PageHeader header{};
+    std:: memcpy(&header, data, sizeof(PageHeader));
+    return header;
+}
+
+
+void Page::setHeader(PageHeader& header){
+    std:: memcpy(data, &header, sizeof(PageHeader));
+}
+
+Page::Slot Page::getSlot(uint16_t slotIndex){
+    PageHeader header= getHeader();
+    if(slotIndex > header.slot_count){
+        throw std::out_of_range("Invalid slot index");
+
+    }
+    Slot slot{};
+    std::size_t offset= sizeof(PageHeader)+slotIndex * sizeof(slot);
+    std:: memcpy(&slot, data+ offset, sizeof(slot) );
+}
